299e770ea09d5319b38b24e1ee1922875f11d888
Oleh Minko <gleb.kurip@gmail.com>
2024-07-31 17:14:25 -0500

    initial pages setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,17 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomeLayout from "./pages/HomeLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+  },
+  {
+    path: "/about",
+    element: <div>About page</div>,
+  },
+]);
+
 export default function App() {
-  return <h1>Jobify App</h1>;
+  return <RouterProvider router={router} />;
 }
